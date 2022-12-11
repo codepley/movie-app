ce88d3435a5bfad94262160f918279e8c8753d19
codepley <officialkushalkaran@gmail.com>
2022-12-11 12:32:19 +0530

    Day one

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1P8f8Oju2FBoq2KBp_2pS5GLumiLUdMg",
+  authDomain: "movie-app-61b77.firebaseapp.com",
+  projectId: "movie-app-61b77",
+  storageBucket: "movie-app-61b77.appspot.com",
+  messagingSenderId: "1023496031411",
+  appId: "1:1023496031411:web:8684710a04ba5e6c6b74c6"
+};
